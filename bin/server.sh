7f655a7f1026b824aa14c8ce4b824d1a9c26bb6b
zhangbaitong <zhangbaitong@163.com>
2017-03-09 11:52:26 +0800

    improve the output and show worker status

--- bin/server.sh
+++ bin/server.sh
@@ -4,15 +4,12 @@ if [ $# -lt 2 ]
 	then
 		echo 'Please check your parameter like [start|stop] [moduleName] [dev|test|pro]'
 		exit
-	else
-		echo $2' module will '$1
 fi
 # init
 php_cmd="php "
 moduleName=$2
 log=$2.out
 env=$3
-echo php commond is $php_cmd
 if [ ! $env ]
 then
     indexFile=index_dev.php
@@ -20,24 +17,38 @@ then
 else
     indexFile=index_$env.php
 fi
+# show info
+echo "PHP CMD : " $php_cmd "\c"
+echo " | Module Name : " $2 "\c"
+echo " | Env Name : " $env
+
+# delay function
+initWorker(){
+    echo $1 "\c"
+    for i in {1..5}  
+    do  
+        sleep 1
+        echo ".\c"
+    done  
+    echo "\n" ......... workers status .........
+    gearadmin --status
+}
+
 # start module
 if [ "$1"  = "start" ]
 then
     echo index file is ./modules/$moduleName/$indexFile
-    echo start[$env] ...
-    #$php_cmd ./modules/$moduleName/index_$env.php
     nohup $php_cmd ./modules/$moduleName/$indexFile &>$log&
-    echo end ...
+    initWorker $1
     exit
 fi
 
 ##stop module
 if [ "$1"  = "stop" ]
 then
-    echo stop[$env] ...
     ps -ef  |grep /modules/$moduleName/$indexFile |grep -v grep |awk '{print $2}'  |while read pid
     do
         kill -9 $pid
     done
-    echo end ...
+    initWorker $1
 fi
