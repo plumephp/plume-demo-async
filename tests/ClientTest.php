fd07ac3bf11420e99aa6da11f2fd8ba124e81b28
zhangbaitong <zhangbaitong@163.com>
2017-03-10 14:15:55 +0800

    improve test useage

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,19 +1,22 @@
 <?php
-//选择需要执行的函数
-fwrite(STDOUT, "Select your Function Number: ".PHP_EOL); 
 //定义函数数据
 $funcs = array(
 	array('key'=>'Example::TestWorker::demoLog','value'=>'HelloWorld'),
 	array('key'=>'Example::TestWorker::demoDB','value'=>'id0'),
 	array('key'=>'Example::TestWorker::demoRedis','value'=>'TestKey'),
 	array('key'=>'Example::TestWorker::demoConfig','value'=>'null'),
-	array('key'=>'Example::TestWorker::demoSys','value'=>'null'));
+	array('key'=>'Example::TestWorker::demoSys','value'=>'null'),
+	array('key'=>'Example::TestWorker::demoDestory','value'=>'null'));
 
 for ($i=0; $i < count($funcs); $i++) {
 	echo '['.$i.'] - '.$funcs[$i]['key'].' - '.$funcs[$i]['value'].PHP_EOL;
 }
+
+//选择需要执行的函数
+fwrite(STDOUT, "Select your Function Number: ".PHP_EOL); 
+
 // 获取选择参数
-$number = trim(fgets(STDIN)); 
+$number = (int)trim(fgets(STDIN));
 
 // 打印提示信息
 fwrite(STDOUT, $funcs[$number]['key']." will be run ...");
