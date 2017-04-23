ba61cea6afb782bcd87d4e3c3b245a304b9b8728
zhangbaitong <zhangbaitong@163.com>
2017-04-23 11:53:21 +0800

    async performance time log support

--- modules/Service/Worker/Log.php
+++ modules/Service/Worker/Log.php
@@ -3,8 +3,11 @@
 namespace Service\Worker;
 
 use Plume\Async\Core\Worker;
+use Elasticsearch\ClientBuilder;
 
 class Log extends Worker{
+
+    private $client;
 
     public function __construct($env){
         parent::__construct(__DIR__, __CLASS__, $env);
@@ -28,5 +31,43 @@ class Log extends Worker{
     	$infoJSON = json_encode($info, JSON_UNESCAPED_UNICODE);
     	$log = "[" . $date . "] - " . $level . " - " . $title . " - " . $infoJSON. "\r\n\r\n";
     	file_put_contents($file, $log, FILE_APPEND); 
+    }
+
+
+    public function asyncTime($job){
+        date_default_timezone_set('UTC');
+        $server_es = empty($this->getConfigValue('server_es')) ? '127.0.0.1:9200' : $this->getConfigValue('server_es');
+        $hosts = [
+            $server_es,
+        ];
+        $this->client = ClientBuilder::create()
+                      ->setRetries(0)
+                      ->setHosts($hosts)
+                      ->build();
+        $msg = $job->workload();
+        $msg_json = json_decode($msg, true);
+
+        $params['body'][] = [
+            'index' => [
+                '_index' => 'plume-'.$msg_json['project'],
+                '_type' => 'plog',
+            ]
+        ];
+        $params['body'][] = [
+            'ip_remote' => $msg_json['ip_remote'],
+            'ip_local' => $msg_json['ip_local'],
+            'project' => $msg_json['project'],
+            'url' => $msg_json['url'],
+            'time_used' => $msg_json['time_used'],
+            'time_create' => getUTCTime(),
+            'context' => $msg_json['context'],
+            'note' => $msg_json['note'],
+            'env' => $msg_json['env'],
+        ];
+        $responses = $this->client->bulk($params);
+    }
+
+    private function getUTCTime(){
+        return date('Y-m-d\TH:i:s\Z');
     }
 }
--- modules/Service/config/dev.php
+++ modules/Service/config/dev.php
@@ -4,4 +4,5 @@ return array(
     'workers' => array(
         'Service::Log::asyncLog' => 1,
     ),
+    'server_es' => '127.0.0.1:9200',
 );
