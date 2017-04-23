37615233d92eb2de624241ffb1b97b67babfea09
zhangbaitong <zhangbaitong@163.com>
2017-04-23 12:09:05 +0800

    es support

--- modules/Service/Worker/Log.php
+++ modules/Service/Worker/Log.php
@@ -34,7 +34,7 @@ class Log extends Worker{
     }
 
 
-    public function asyncTime($job){
+    public function ($job){
         date_default_timezone_set('UTC');
         $server_es = empty($this->getConfigValue('server_es')) ? '127.0.0.1:9200' : $this->getConfigValue('server_es');
         $hosts = [
